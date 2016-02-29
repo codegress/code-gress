6d51320f33ab457ec7e85b3514b211001b2661b0
iMinion <k.srirammanoj@gmail.com>
2016-02-29 10:37:04 +0530

    Initial Commit after a long way

---
+++ main.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import endpoints
+from protorpc import remote
+
+from google.appengine.ext import ndb
+
+from models import Testcase
+from models import Question
+from models import TestcaseForm
+from models import QuestionForm
+from models import QuestionForms
+from models import TestcaseForms
+
+
+EMAIL_SCOPE = endpoints.EMAIL_SCOPE
+API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
+WEB_CLIENT_ID='602783917304-5epdo5rivihkj4c6oe62a8vf863kuk3r.apps.googleusercontent.com'
+
+
+QUES_GET_REQUEST = endpoints.ResourceContainer(
+    message_types.VoidMessage,)
+
+
+@endpoints.api(name='codegress', version='v2', 
+	allowed_client_ids=[WEB_CLIENT_ID, API_EXPLORER_CLIENT_ID],
+	scopes=[EMAIL_SCOPE])
+class CodegressApi(remote.Service):
+	
+
+	def _addQuestionObject(self, request):
+		user = endpoints.get_current_user()
+		if not user:
+			raise endpoints.UnauthorizedException("Not an anuthorized user")
+		data = {field.name: getattr(request, field.name) for field in request.all_fields()}
+		testcases = data['sample_testcase'] + data['testcases']
+		del data['sample_testcase']
+		del data['testcases']
+		tc = []
+		for test in testcases:
+			tc.append(Testcase(iput=test.iput,
+												 oput=test.oput,
+												 points=test.points,
+												 hint=test.hint))
+		q_key = ndb.Key(Question, data['title'])
+		data['key'] = q_key
+		data['author'] = user
+		Question(**data).put()
+		t_ids = Testcase.allocate_ids(size=len(tc), parent=q_key)
+		for i in range(0, len(t_ids)):
+			t_key = ndb.Key(Testcase, t_ids[i], parent = q_key)
+			tc[i].put()
+		return request
+
+	@endpoints.method(QuestionForm, QuestionForm, 
+		path='addQuestion',
+		http_method='POST',
+		name='addQuestion')
+	def addQuestion(self, request):
+		return self._addQuestionObject(request)
+
+
+
+	@endpoints.method(QUES_GET_REQUEST, )
+
+
+api = endpoints.api_server([CodegressApi]) # register API
---
+++ models.py
@@ -0,0 +1,46 @@
+
+
+import endpoints
+from protorpc import messages
+from google.appengine.ext import ndb
+
+class Testcase(ndb.Model):
+	iput = ndb.StringProperty(required = True)
+	oput = ndb.StringProperty(required = True)
+	points = ndb.IntegerProperty(required = True)
+	hint = ndb.StringProperty(default = None)
+
+
+class Question(ndb.Model):
+	title = ndb.StringProperty(required = True)
+	description = ndb.TextProperty(required = True)
+	image = ndb.StringProperty(repeated = True)
+	score = ndb.IntegerProperty()
+	author = ndb.UserProperty(required = True)
+
+
+class TestcaseForm(messages.Message):
+	iput = messages.StringField(1, required=True)
+	oput = messages.StringField(2, required=True)
+	points = messages.IntegerField(3, required=True)
+	hint = messages.StringField(4)
+
+
+class QuestionForm(messages.Message):
+	title = messages.StringField(1, required=True)
+	description = messages.StringField(2, required=True)
+	sample_testcase = messages.MessageField(TestcaseForm, 3, repeated=True)
+	testcases = messages.MessageField(TestcaseForm, 4, repeated=True)
+	image = messages.StringField(5, repeated=True)
+	# websafeKey      = messages.StringField(6)
+
+class QuestionMiniForm(messages.Message):
+	title = messages.StringField(1, required=True)
+	handle = messages.StringField(2, required=True)
+
+
+class QuestionForms(messages.Message):
+	items = messages.MessageField(QuestionForm, 1, repeated=True)
+
+class TestcaseForms(messages.Message):
+	items = messages.MessageField(TestcaseForm, 1, repeated=True)
